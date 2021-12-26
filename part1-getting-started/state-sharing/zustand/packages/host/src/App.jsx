8703035b966c4a306b2a948d1bb7f06be15df371
Jack Herrington <jherr@pobox.com>
2021-12-26 10:26:27 -0800

    Zustand small update

--- part1-getting-started/state-sharing/zustand/packages/host/src/App.jsx
+++ part1-getting-started/state-sharing/zustand/packages/host/src/App.jsx
@@ -1,8 +1,7 @@
 import React, { useState } from "react";
 import ReactDOM from "react-dom";
 
-const Header = React.lazy(() => import("my-nav/Header"));
-
+import Header from "my-nav/Header";
 import { useCount } from "host/store";
 
 import "./index.scss";
@@ -11,9 +10,7 @@ const App = () => {
   const { count, increment } = useCount();
   return (
     <div className="mt-10 text-3xl mx-auto max-w-6xl">
-      <React.Suspense fallback={<div />}>
-        <Header />
-      </React.Suspense>
+      <Header />
       <div className="mt-10">Hi there, I'm some cool product.</div>
       <button
         className="px-5 py-2 bg-green-500 text-white rounded-xl"
